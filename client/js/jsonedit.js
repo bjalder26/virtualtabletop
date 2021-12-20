db6cd48769efdc4fce8de0b6fcf0b9755d710406
bjalder26 <78324099+bjalder26@users.noreply.github.com>
2021-12-20 03:07:58 -0600

    Populate JSON editor options

--- client/js/jsonedit.js
+++ client/js/jsonedit.js
@@ -573,6 +573,7 @@ function jeAddCommands() {
   widgetTypes.push(jeAddWidgetPropertyCommands(new Holder()));
   widgetTypes.push(jeAddWidgetPropertyCommands(new Label()));
   widgetTypes.push(jeAddWidgetPropertyCommands(new Pile()));
+  widgetTypes.push(jeAddWidgetPropertyCommands(new Richtext()));
   widgetTypes.push(jeAddWidgetPropertyCommands(new Seat()));
   widgetTypes.push(jeAddWidgetPropertyCommands(new Spinner()));
   widgetTypes.push(jeAddWidgetPropertyCommands(new Timer()));
