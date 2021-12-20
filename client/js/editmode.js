e7171c9024dd68da1c97c30b716d08a6d89acf82
Brandon Lewis <brandon@mousewax.com>
2021-12-20 10:24:20 -0800

    Added basic HTML editor. It still crashes when attempting to apply styles.

--- client/js/editmode.js
+++ client/js/editmode.js
@@ -99,6 +99,68 @@ function populateEditOptionsButton(widget) {
   }
 }
 
+function populateEditOptionsRichtext(widget) {
+  $('#richtextText').innerHTML = widget.text || "~ no text found ~";
+  $('#richtextWidth').value = widget.width||100;
+  $('#richtextHeight').value = widget.height||20;
+  $('#richtextWidthNumber').value = widget.width||100;
+  $('#richtextHeightNumber').value = widget.height||20;
+  $('#richtextEditable').checked = widget.editable;
+  initRichtextEditor();
+}
+
+var oDoc, sDefTxt;
+
+function initRichtextEditor() {
+  oDoc = document.getElementById("richtextText");
+  sDefTxt = oDoc.innerHTML;
+  if (document.compForm.switchMode.checked) { setDocMode(true); }
+}
+
+function formatDoc(sCmd, sValue) {
+  if (validateMode()) { document.execCommand(sCmd, false, sValue); oDoc.focus(); }
+}
+
+function validateMode() {
+  if (!document.compForm.switchMode.checked) { return true ; }
+  alert("Uncheck \"Show HTML\".");
+  oDoc.focus();
+  return false;
+}
+
+function setDocMode(bToSource) {
+  var oContent;
+  if (bToSource) {
+    oContent = document.createTextNode(oDoc.innerHTML);
+    oDoc.innerHTML = "";
+    var oPre = document.createElement("pre");
+    oDoc.contentEditable = false;
+    oPre.id = "sourceText";
+    oPre.contentEditable = true;
+    oPre.appendChild(oContent);
+    oDoc.appendChild(oPre);
+    document.execCommand("defaultParagraphSeparator", false, "div");
+  } else {
+    if (document.all) {
+      oDoc.innerHTML = oDoc.innerText;
+    } else {
+      oContent = document.createRange();
+      oContent.selectNodeContents(oDoc.firstChild);
+      oDoc.innerHTML = oContent.toString();
+    }
+    oDoc.contentEditable = true;
+  }
+  oDoc.focus();
+}
+
+function printDoc() {
+  if (!validateMode()) { return; }
+  var oPrntWin = window.open("","_blank","width=450,height=470,left=400,top=100,menubar=yes,toolbar=no,location=no,scrollbars=yes");
+  oPrntWin.document.open();
+  oPrntWin.document.write("<!doctype html><html><head><title>Print<\/title><\/head><body onload=\"print();\">" + oDoc.innerHTML + "<\/body><\/html>");
+  oPrntWin.document.close();
+}
+
 function applyEditOptionsButton(widget) {
   if ($('#buttonText').value=="~ no text found ~")
     delete widget.text;
@@ -424,6 +486,8 @@ function editClick(widget) {
     populateEditOptionsBasic(widget.state);
   if(type == 'button')
     populateEditOptionsButton(widget.state);
+  if(type == 'richtext')
+    populateEditOptionsRichtext(widget.state);
   if(type == 'canvas')
     populateEditOptionsCanvas(widget.state);
   if(type == 'holder')
